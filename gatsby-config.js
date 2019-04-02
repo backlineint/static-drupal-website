358b8feea2bf666c6c88aefd38e8724341ec3793
Brian Perry <contact.brianperry@gmail.com>
2019-04-02 13:25:51 -0500

    Initial typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,5 +47,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Overpass', 'Source Sans Pro:900']
+        }
+      }
+    }
   ],
 }
--- src/components/ResourceMenu.js
+++ src/components/ResourceMenu.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
 
+import styles from "./ResourceMenu.module.css"
+
 export default () => (
   <StaticQuery
     query={graphql`
@@ -25,10 +27,11 @@ export default () => (
     `}
     render={data => (
       <>
-        <ul>
+        <ul className={styles.list}>
           {data.allMarkdownRemark.edges.map(({node}) => (
             <Link
               to={node.fields.slug}
+              class={styles.link}
             >
               <li>{node.frontmatter.title}</li>
             </Link>
